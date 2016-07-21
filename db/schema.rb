d37a4e1a391128101a42fa15734ab9c51d58a30b
명세윤 <myeongseyoon@myeongseyun-ui-MacBook-Pro.local>
2016-07-21 20:07:20 +0900

    user profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716064437) do
+ActiveRecord::Schema.define(version: 20160721081833) do
 
   create_table "stocks", force: :cascade do |t|
     t.string   "ticker"
@@ -41,10 +41,8 @@ ActiveRecord::Schema.define(version: 20160716064437) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "image"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
