0c6157c288946eb20e32d238f661492b6695edf1
명세윤 <myeongseyoon@myeongseyun-ui-MacBook-Pro.local>
2016-07-22 19:43:39 +0900

    i got an error on ajax

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,5 @@
 class StocksController < ApplicationController
-  
+
   def search
     if params[:stock]
       @stock = Stock.find_by_ticker(params[:stock])
@@ -7,7 +7,7 @@ class StocksController < ApplicationController
     end
 
     if @stock
-        render partial: 'lookup'
+        render partial: 'stocks/lookup'
     else
       render status: :not_found, nothing: true
     end
